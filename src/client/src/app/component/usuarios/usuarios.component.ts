85ce442d5e01a6453e44659b7b41d6c5b6a2038d
Diegue83 <135059873+Diegue83@users.noreply.github.com>
2024-10-17 19:44:23 -0600

    registro

--- src/client/src/app/component/usuarios/usuarios.component.ts
+++ src/client/src/app/component/usuarios/usuarios.component.ts
@@ -15,6 +15,7 @@ export class UsuariosComponent implements OnInit {
   rol: string = '';
   fechaRegistro: string = ''; // Lo manejaremos como string para el formulario
   usuarioSeleccionado: Usuarios | null = null;
+  userType='student';
 
   // Almacenar la lista de usuarios
   usuarios: Usuarios[] = [];
