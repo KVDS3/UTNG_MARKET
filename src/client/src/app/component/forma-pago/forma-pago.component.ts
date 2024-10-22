31bbf5e7cf1ef23824f57283d895f8b4e58fab29
KVDS3 <kconejosalazar@gmail.com>
2024-10-22 17:02:18 -0600

    .

--- src/client/src/app/component/forma-pago/forma-pago.component.ts
+++ src/client/src/app/component/forma-pago/forma-pago.component.ts
@@ -1,34 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+declare var paypal: any;
+
 @Component({
   selector: 'app-forma-pago',
   templateUrl: './forma-pago.component.html',
-  styleUrl: './forma-pago.component.css'
+  styleUrls: ['./forma-pago.component.css'] // Cambié "styleUrl" por "styleUrls"
 })
 export class FormaPagoComponent implements OnInit {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef | undefined;
 
   constructor(private http: HttpClient) {}
 
+  productos = {
+    descripcion: 'si',
+    precio: '19',
+    img: 'img pro'
+  }
+
   ngOnInit(): void {
-    // Inicializa el SDK de Mercado Pago con tu clave pública
-    const mp = new (window as any).MercadoPago('TEST-a34bb4f8-e2b0-447c-8a72-e31ffa215c17'); // Reemplaza con tu clave pública de Mercado Pago
-
-    // Obtener el ID de la preferencia desde el backend
-    this.http.get('http://localhost:3000/api/mercado-pago/preferencia').subscribe((response: any) => {
-      const preferenceId = response.preferenceId; // Asegúrate de que el backend devuelva el preferenceId
-
-      // Inicializa el checkout usando el ID de la preferencia
-      mp.bricks().create('wallet', 'wallet_container', {
-        initialization: {
-          preferenceId: preferenceId, // Utiliza el preferenceId generado
+    paypal
+      .Buttons({
+        createOrder: (data: any, actions: any) => {
+          return actions.order.create({
+            purchase_units: [{
+              descripcion: this.productos.descripcion,
+              amount: {
+                currency_code: 'MXN',
+                value: this.productos.precio
+              }
+            }]
+          });
         },
-        customization: {
-          texts: {
-            valueProp: 'smart_option',
-          },
+        onApprove: async (data: any, actions: any) => {
+          const order = await actions.order.capture();
+          console.log(order);
         },
-      });
-    });
+        onError: (err: any) => {
+          console.log(err);
+        }
+      })
+      .render(this.paypalElement?.nativeElement);
   }
 }
- 
